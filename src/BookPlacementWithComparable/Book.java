697d597f1fe2f789a7f92985a1b815f0ecbf61d1
Berk <mberktuncer@gmail.com>
2024-10-29 22:45:25 +0300

    Book Placement With Comparable added.

--- src/BookPlacementWithComparable/Book.java
+++ src/BookPlacementWithComparable/Book.java
@@ -1,4 +1,33 @@
 package BookPlacementWithComparable;
 
-public class Book {
+public class Book implements Comparable {
+
+    String name;
+    int pageCount;
+    String authorName;
+    String publishDate;
+
+    public Book(String name, int page, String authorName, String publishDate) {
+        this.name = name;
+        this.pageCount = page;
+        this.authorName = authorName;
+        this.publishDate = publishDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{name='" + name + "', Page Number='" + pageCount + "', Author Name=" + authorName + "', Publish Date=" + publishDate + '}';
+    }
+
+    /*@Override
+    public int compareTo(Object o) {
+        Book other = (Book) o;
+        return this.name.compareTo(other.name);
+    }*/
+
+    @Override
+    public int compareTo(Object o) {
+        Book other = (Book) o;
+        return Integer.compare(this.pageCount, other.pageCount);
+    }
 }
--- src/BookPlacementWithComparable/Main.java
+++ src/BookPlacementWithComparable/Main.java
@@ -1,9 +1,29 @@
 package BookPlacementWithComparable;
 
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Book book1 = new Book("Java", 130, "Mustafa", "12.12.12");
+        Book book2 = new Book("Python", 200, "Berk", "12.12.12");
+        Book book3 = new Book("C#", 120, "Tuncer", "12.12.12");
+        Book book4 = new Book("AI", 100, "Joe", "12.12.12");
+        Book book5 = new Book("Go", 150, "Josh", "12.12.12");
+
+        Set<Book> books = new TreeSet<>();
+        books.add(book1);
+        books.add(book2);
+        books.add(book3);
+        books.add(book4);
+        books.add(book5);
+
+        for (Book book : books){
+            System.out.println(book.toString());
+        }
 
     }
 
